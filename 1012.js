a253b8d43b79b401308f5e8a0f8859b61b5218e6
isabella bonjani <isabella.bonjani@gmail.com>
2025-03-28 20:09:05 -0300

    exercicios novos

--- 1012.js
+++ 1012.js
@@ -1,8 +1,17 @@
 var input = require('fs').readFileSync('entrada.txt', 'utf8');
 var lines = input.split('\n');
 let valores = lines[0].split(" ");
-let a =  parseFloat(valores[1]);
+let a =  parseFloat(valores[0]);
 let b =  parseFloat(valores[1]);
-let c =  parseFloat(valores[1]);
+let c =  parseFloat(valores[2]);
+const pi=3.14159;
 let resolucaoLetraA = (a*c)/2;
-console.log ('VOLUME = '+resolucaoLetraA.toFixed(3));
+let resolucaoLetraB = pi*(c*c)
+let resolucaoLetraC = (a+b)*c/2
+let resolucaoLetraD = b*b
+let resolucaoLetraE = a*b
+console.log ('TRIANGULO: '+resolucaoLetraA.toFixed(3));
+console.log ('CIRCULO: '+resolucaoLetraB.toFixed(3));
+console.log ('TRAPEZIO: '+resolucaoLetraC.toFixed(3));
+console.log ('QUADRADO: '+resolucaoLetraD.toFixed(3));
+console.log ('RETANGULO: '+resolucaoLetraE.toFixed(3));
